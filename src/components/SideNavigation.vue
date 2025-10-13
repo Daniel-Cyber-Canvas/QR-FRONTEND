bb90fba3a9ecbd577eeb7bf3393d67d01a88c9be
Daniel Seta <intern@cybercanvas.co.zm>
2025-10-13 16:47:43 +0200

    feat: enhance SideNavigation component with improved layout, new menu items, and mobile header

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -1,84 +1,126 @@
 <template>
     <div class="flex h-screen">
         <div class="md:hidden fixed inset-0 z-10" v-if="showMenu" @click="showMenu = false"></div>
-        <aside
-            class="fixed left-0 top-0 h-screen w-[164px] transition-transform duration-300 md:translate-x-0 -translate-x-full z-20"
-            :class="{ '-translate-x-full': !showMenu, 'translate-x-0': showMenu }">
-            <div class="h-full w-full flex flex-col" style="box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.25)">
-                <div class="navigation">
-                    <div
-                        class="bg-[#ffffff] w-full flex flex-col items-center justify-start gap-[15px] shrink-0 w-[164px] relative">
-                        <img class="shrink-0 w-[118px] h-10 relative" alt="" src="@/assets/images/logo.png" />
 
-                        <!-- Menu items -->
-                        <div class="flex flex-col gap-2.5 items-center justify-start shrink-0 relative">
+        <!-- Compact Sidebar -->
+        <aside
+            class="fixed left-0 top-0 h-screen transition-transform duration-300 md:translate-x-0 -translate-x-full z-20"
+            :class="{ '-translate-x-full': !showMenu, 'translate-x-0': showMenu }">
+            <div class="pr-1 flex flex-row gap-0 items-center justify-start shrink-0 relative h-full">
+                <div
+                    class="bg-[#ffffff] border-solid border-[#e2e8f0] border-r pt-2.5 pb-2.5 flex flex-col gap-[15px] items-center justify-start shrink-0 relative h-full"
+                    style="">
+
+                    <!-- Logo -->
+                    <div class="pr-[7px] pl-[7px] flex flex-col gap-2.5 items-start justify-start self-stretch shrink-0 relative">
+                        <img class="self-stretch shrink-0 h-[34px] relative" style="object-fit: contain" src="@/assets/images/logo.png" alt="Logo" />
+                    </div>
+
+                    <!-- Navigation Container -->
+                    <div
+                        class="border-solid border-[#e2e8f0] border-t p-2.5 flex flex-col items-center justify-between shrink-0 flex-1 relative"
+                        style="box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.25)">
+
+                        <!-- Main Menu Items -->
+                        <div class="flex flex-col gap-[7px] items-start justify-start shrink-0 relative">
                             <router-link to="/" exact-active-class="active-menu-item"
-                                class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                                <Icon icon="ph:squares-four"
-                                    class="shrink-0 text-[#424242] w-[58px] h-[60px] relative object-cover" />
-                                <div
-                                    class="text-[#000000] text-left font-['Inter-Medium',_sans-serif] text-xs font-medium relative">
-                                    DashBoard
+                                class="menu-item rounded-[3px] pr-1.5 pl-1.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                                <Icon icon="ph:squares-four" class="shrink-0 w-4 h-4" />
+                                <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
+                                    Dashboard
                                 </div>
                             </router-link>
 
                             <router-link to="/services" exact-active-class="active-menu-item"
-                                class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                                <Icon icon="ph:stack"
-                                    class="shrink-0 text-[#424242] w-[58px] h-[60px] relative object-cover" />
-                                <div
-                                    class="flex-1 relative text-xs font-medium inline-block overflow-hidden text-ellipsis whitespace-nowrap h-[22px] flex items-center pl-[2px]">
+                                class="menu-item rounded-[3px] pr-1.5 pl-1.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                                <Icon icon="ph:stack" class="shrink-0 w-4 h-4" />
+                                <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
                                     Services
                                 </div>
                             </router-link>
 
                             <router-link to="/domains" exact-active-class="active-menu-item"
-                                class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                                <Icon icon="ph:globe"
-                                    class="shrink-0 text-[#424242] w-[58px] h-[60px] relative object-cover" />
-                                <div
-                                    class="flex-1 relative text-xs font-medium inline-block overflow-hidden text-ellipsis whitespace-nowrap h-[22px] flex items-center pl-[2px]">
+                                class="menu-item rounded-[3px] pr-1.5 pl-1.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                                <Icon icon="ph:globe" class="shrink-0 w-4 h-4" />
+                                <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
                                     Domains
                                 </div>
                             </router-link>
 
                             <router-link to="/billing" exact-active-class="active-menu-item"
-                                class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                                <Icon icon="ph:credit-card"
-                                    class="shrink-0 text-[#424242] w-[58px] h-[60px] relative object-cover" />
-                                <div
-                                    class="flex-1 relative text-xs font-medium inline-block overflow-hidden text-ellipsis whitespace-nowrap h-[22px] flex items-center pl-[2px]">
+                                class="menu-item rounded-[3px] pr-1.5 pl-2.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                                <Icon icon="ph:credit-card" class="shrink-0 w-4 h-4" />
+                                <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
                                     Billing
                                 </div>
                             </router-link>
 
-                            <router-link to="/support" exact-active-class="active-menu-item"
-                                class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                                <Icon icon="ph:chat-circle-dots"
-                                    class="shrink-0 text-[#424242] w-[58px] h-[60px] relative object-cover" />
-                                <div
-                                    class="flex-1 relative text-xs font-medium inline-block overflow-hidden text-ellipsis whitespace-nowrap h-[22px] flex items-center pl-[2px]">
-                                    Support
+                            <router-link to="/projects" exact-active-class="active-menu-item"
+                                class="menu-item rounded-[3px] pr-1.5 pl-1.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                                <Icon icon="ph:folder" class="shrink-0 w-4 h-4" />
+                                <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
+                                    Projects
                                 </div>
                             </router-link>
                         </div>
-                    </div>
 
-                    <!-- Account section -->
-                    <div class="flex flex-col gap-[34px] items-center justify-end shrink-0 h-[191px] relative">
-                        <div
-                            class="menu-item flex flex-col gap-1 items-center justify-start shrink-0 w-36 h-[88px] cursor-pointer">
-                            <Icon icon="ph:user" class="shrink-0 w-[58px] h-[60px] relative object-cover" />
-                            <div
-                                class="text-[#000000] text-left font-['Inter-Regular',_sans-serif] text-xs font-normal relative">
-                                My Account
+                        <!-- Bottom Support Item -->
+                        <router-link to="/support" exact-active-class="active-menu-item"
+                            class="menu-item rounded-[3px] pr-1.5 pl-1.5 flex flex-row gap-1 items-center justify-start shrink-0 w-40 h-[23px] relative">
+                            <Icon icon="ph:chat-circle-dots" class="shrink-0 w-4 h-4" />
+                            <div class="text-[#212121] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative flex items-center justify-start">
+                                Support
                             </div>
-                        </div>
+                        </router-link>
                     </div>
                 </div>
             </div>
         </aside>
-        <div class="flex-1 flex flex-col items-start justify-start z-1 relative md:pl-[164px]">
+
+        <!-- Main Content Area -->
+        <div class="flex-1 flex flex-col items-start justify-start z-1 relative md:pl-[190px]">
+            <!-- Desktop Header -->
+            <div class="hidden md:flex flex-col gap-2.5 items-start justify-start self-stretch shrink-0 relative"
+                style="">
+                <div class="bg-[] pr-2.5 pl-2.5 flex flex-row items-center justify-between self-stretch shrink-0 relative overflow-hidden">
+                    <!-- Notification Banner (Left) -->
+                    <div class="p-2.5 flex flex-row gap-2.5 items-center justify-center shrink-0 relative">
+                        <div class="flex flex-row gap-2.5 items-start justify-end shrink-0 relative">
+                            <div class="text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative">
+                                <span>
+                                    <span class="text-[#000000]">Hosting for </span>
+                                    <span class="text-[#0c768a] font-semibold">maimo.com</span>
+                                    <span class="text-[#000000]"> is about to expire in 15 Days - </span>
+                                    <span class="text-[#0c768a] font-semibold cursor-pointer hover:underline">Renew now</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="flex flex-row gap-2.5 items-center justify-start shrink-0 relative cursor-pointer">
+                            <Icon icon="ph:x" class="shrink-0 w-[15px] h-[15px]" />
+                        </div>
+                    </div>
+
+                    <!-- User Info (Right) -->
+                    <div class="flex flex-row gap-0 items-center justify-center shrink-0 relative">
+                        <div class="p-2.5 flex flex-row gap-2.5 items-center justify-center shrink-0 relative">
+                            <div class="text-[#000000] text-left font-['Manrope-Regular',_sans-serif] text-xs font-normal relative">
+                                Hello, {{ userData?.name || 'User' }}
+                            </div>
+                        </div>
+                        <div class="p-2.5 flex flex-row gap-2.5 items-center justify-start shrink-0 relative cursor-pointer">
+                            <Icon icon="ph:bell" class="shrink-0 w-5 h-5" />
+                        </div>
+                        <div class="pl-2.5 flex flex-row gap-2.5 items-center justify-start shrink-0 relative">
+                            <div class="bg-[#d9d9d9] rounded-[50%] shrink-0 w-9 h-9 relative"></div>
+                        </div>
+                        <div class="p-2.5 flex flex-row gap-2.5 items-center justify-start shrink-0 relative cursor-pointer">
+                            <Icon icon="ph:caret-down" class="shrink-0 w-3.5 h-3.5" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Mobile Header -->
             <div
                 class="md:hidden flex items-center justify-between shadow-[0px_0px_2px_rgba(0,_0,_0,_0.25)] bg-darkslateblue-100 fixed top-0 left-0 right-0 z-30">
                 <img class="ml-[15px] relative h-[43px] object-cover" alt="" src="@/assets/images/logo.png" />
@@ -94,6 +136,7 @@
                     </svg>
                 </button>
             </div>
+
             <div class="main-content self-stretch flex-1 mt-[10px] md:mt-0">
                 <slot></slot>
             </div>
@@ -172,36 +215,35 @@ export default {
 </script>
 
 <style scoped>
-.navigation {
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100%;
-    background-color: #fff;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px;
-    box-sizing: border-box;
-    gap: 18px;
-    overflow-y: auto;
-}
-
 /* Menu Item Styles */
 .menu-item {
     background-color: transparent;
-    transition: background-color 0.3s ease;
-    border-radius: 4px;
+    transition: background-color 0.3s ease, color 0.3s ease;
 }
 
 .menu-item:hover {
-    background-color: rgba(38, 102, 111, 0.13);
+    background-color: #dcebed;
+}
+
+.menu-item:hover div {
+    color: #212121;
+}
+
+.menu-item:hover :deep(svg) {
+    color: #212121;
 }
 
 /* Active state styling */
 .active-menu-item {
-    background-color: rgba(38, 102, 111, 0.13);
+    background-color: #0c768a !important;
+}
+
+.active-menu-item div {
+    color: #ffffff !important;
+}
+
+.active-menu-item :deep(svg) {
+    color: #ffffff !important;
 }
 
 .show {
